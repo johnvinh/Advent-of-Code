4e6e06ea53885a59f9b74205861c6170bd775be4
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-30 13:12:36 -0500

    solve day 1 part 2

--- src/twenty_fifteen/day_1.rs
+++ src/twenty_fifteen/day_1.rs
@@ -1,15 +1,30 @@
-pub fn get_floor_difference(c: char) -> i32 {
-    match c {
-        '(' => 1,
-        ')' => -1,
-        _ => 0
+pub mod part_1 {
+    pub fn get_floor_difference(c: char) -> i32 {
+        match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0
+        }
+    }
+
+    pub fn solve(input: &String) -> i32 {
+        let mut floor: i32 = 0;
+        for character in input.chars() {
+            floor += get_floor_difference(character);
+        }
+        floor
     }
 }
 
-pub fn solve(input: String) -> i32 {
-    let mut floor: i32 = 0;
-    for character in input.chars() {
-        floor += get_floor_difference(character);
+pub mod part_2 {
+    pub fn solve(input: &String) -> i32 {
+        let mut floor: i32 = 0;
+        for (i, character) in input.chars().enumerate() {
+            floor += super::part_1::get_floor_difference(character);
+            if floor == -1 {
+                return (i + 1) as i32;
+            }
+        }
+        -1
     }
-    floor
 }

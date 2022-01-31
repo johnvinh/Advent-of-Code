6b9a323004c080edf045bc80516101b496a6753d
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-30 22:33:23 -0500

    made 2015 day 2 less repetitive

--- src/twenty_fifteen/day_2.rs
+++ src/twenty_fifteen/day_2.rs
@@ -6,6 +6,11 @@ pub struct Present {
     height: i32,
 }
 
+pub enum Part {
+    Part1,
+    Part2,
+}
+
 pub mod part_1 {
     pub fn calculate_surface_area(present: &super::Present) -> i32 {
         (2 * present.length * present.width) + (2 * present.width * present.height) + (2 * present.height * present.length)
@@ -17,20 +22,6 @@ pub mod part_1 {
         let height_length: i32 = present.height * present.length;
         // Return the minimum of the 3 values
         super::cmp::min(super::cmp::min(length_width, width_height), height_length)
-    }
-
-    pub fn solve(input: Vec<&str>) -> i32 {
-        let mut sum: i32 = 0;
-        for line in input {
-            let dimensions: Vec<&str> = line.split("x").collect();
-            let present: super::Present = super::Present {
-                width: dimensions[0].trim().parse().expect("Failed to read width"),
-                height: dimensions[1].trim().parse().expect("Failed to read length"),
-                length: dimensions[2].trim().parse().expect("Failed to read height"),
-            };
-            sum += calculate_surface_area(&present) + calculate_extra(&present);
-        }
-        sum
     }
 }
 
@@ -44,18 +35,21 @@ pub mod part_2 {
         measures.sort();
         (2 * measures[0]) + (2 * measures[1])
     }
+}
 
-    pub fn solve(input: Vec<&str>) -> i32 {
-        let mut sum: i32 = 0;
-        for line in input {
-            let dimensions: Vec<&str> = line.split("x").collect();
-            let present: super::Present = super::Present {
-                width: dimensions[0].trim().parse().expect("Failed to read width"),
-                height: dimensions[1].trim().parse().expect("Failed to read height"),
-                length: dimensions[2].trim().parse().expect("Failed to read length"),
-            };
-            sum += calculate_cubic_volume(&present) + calculate_smallest_perimeter(&present);
+pub fn solve(input: Vec<&str>, part: &Part) -> i32 {
+    let mut sum: i32 = 0;
+    for line in input {
+        let dimensions: Vec<&str> = line.split("x").collect();
+        let present: Present = Present {
+            width: dimensions[0].trim().parse().expect("Failed to read width"),
+            height: dimensions[1].trim().parse().expect("Failed to read height"),
+            length: dimensions[2].trim().parse().expect("Failed to read length"),
+        };
+        match part {
+            Part::Part1=> sum += part_1::calculate_surface_area(&present) + part_1::calculate_extra(&present),
+            Part::Part2=> sum += part_2::calculate_cubic_volume(&present) + part_2::calculate_smallest_perimeter(&present),
         }
-        sum
     }
+    sum
 }
